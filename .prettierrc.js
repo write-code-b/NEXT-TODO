385b72fa6751f515ea60e850cc7f55377d9ad9be
write-code-b <minjichoi.official@gmail.com>
2025-02-11 16:15:14 +0900

    chore: prettier style settings update

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  singleQuote: true,
-  printWidth: 120,
-  tabWidth: 2,
-  trailingComma: "none",
-  jsxBracketSameLine: true,
-  jsxSingleQuote: true,
-  arrowParens: "avoid",
-};
