b58d0bb01fe8172621c0314cc87718cf8f34b643
write-code-b <minjichoi.official@gmail.com>
2025-02-11 16:21:17 +0900

    feat: add update isCompleted of checklist action

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,53 +1,75 @@
-'use server';
+'use server'
 
-import { redirect } from 'next/navigation';
-import { z } from 'zod'; //validation library
+import { redirect } from 'next/navigation'
+import { revalidatePath } from 'next/cache' //Since you're updating the data displayed in the invoices route, you want to clear this cache and trigger a new request to the server.
+import { z } from 'zod' //validation library
 
 // Use Zod to update the expected types
 const FormSchema = z.object({
-  name: z.string()
-});
+  name: z.string(),
+})
 
-const ToDo = FormSchema.omit({});
+const ToDo = FormSchema.omit({})
 
 export type State = {
   errors?: {
-    name?: string[];
-  };
-  message?: string | null;
-};
+    name?: string[]
+  }
+  message?: string | null
+}
 
 // Post Todo data
 export const createTodo = async (prevState: State, formData: FormData) => {
   // Validate form using Zod
   const validatedFields = ToDo.safeParse({
-    name: formData.get('name')
-  });
+    name: formData.get('name'),
+  })
 
   if (!validatedFields.success) {
     return {
       errors: validatedFields.error.flatten().fieldErrors,
-      message: 'Missing Fields. Failed to Create Todo.'
-    };
+      message: 'Missing Fields. Failed to Create Todo.',
+    }
   }
 
-  const { name } = validatedFields.data;
+  const { name } = validatedFields.data
 
-  const url = `${process.env.NEXT_PUBLIC_API_KEY}/${process.env.NEXT_PUBLIC_TENANT_ID}/items`;
+  const url = `${process.env.NEXT_PUBLIC_API_KEY}/${process.env.NEXT_PUBLIC_TENANT_ID}/items`
 
   try {
     await fetch(url, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ name })
-    });
+      body: JSON.stringify({ name }),
+    })
   } catch (error) {
     return {
-      message: `Failed to Create Todo. Error: ${error}`
-    };
+      message: `Failed to Create Todo. Error: ${error}`,
+    }
   }
 
-  redirect('/'); //Redirect to home
-};
+  redirect('/') //Redirect to home
+}
+
+// Patch Todo isCompleted
+export const updateTodo = async (id: number, isCompleted: boolean) => {
+  const url = `${process.env.NEXT_PUBLIC_API_KEY}/${process.env.NEXT_PUBLIC_TENANT_ID}/items/${id}`
+
+  try {
+    fetch(url, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ isCompleted }),
+    })
+  } catch (error) {
+    return {
+      message: `[Todo] Failed to Update isCompleted. Error: ${error}`,
+    }
+  }
+
+  revalidatePath('/') //서버 컴포넌트의 새로고침 없이 변경된 부분을 적용
+}
