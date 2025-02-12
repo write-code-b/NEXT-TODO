6abcca92d9ce1685544502da1856f76d5d75f95d
write-code-b <minjichoi.official@gmail.com>
2025-02-12 21:21:56 +0900

    chore: update default design when checklist is empty

--- app/ui/List.tsx
+++ app/ui/List.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Todo from '@/ui/Todo'
 import styles from '@/styles/List.module.scss'
 
@@ -18,10 +19,44 @@ export default function List(props: ListProps) {
   return (
     <div className={styles.list}>
       {label === 'TODO' && (
-        <div className={`${styles.tag} ${styles.todo}`}>{label}</div>
+        <>
+          <div className={`${styles.tag} ${styles.todo}`}>{label}</div>
+          {task.length === 0 && (
+            <>
+              <Image
+                src="/default/default-todo.svg"
+                width={240}
+                height={240}
+                alt="no todo data"
+              />
+              <div className={styles.emptyMessage}>
+                할 일이 없어요.
+                <br />
+                TODO를 새롭게 추가해주세요!
+              </div>
+            </>
+          )}
+        </>
       )}
       {label === 'DONE' && (
-        <div className={`${styles.tag} ${styles.done}`}>{label}</div>
+        <>
+          <div className={`${styles.tag} ${styles.done}`}>{label}</div>
+          {task.length === 0 && (
+            <>
+              <Image
+                src="/default/default-done.svg"
+                width={240}
+                height={240}
+                alt="no done data"
+              />
+              <div className={styles.emptyMessage}>
+                아직 다 한 일이 없어요.
+                <br />
+                해야 할 일을 체크해보세요!
+              </div>
+            </>
+          )}
+        </>
       )}
       {task?.map((todo) => {
         return (
