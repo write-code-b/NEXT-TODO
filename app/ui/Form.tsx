7ff12e6b944e6ae42778e202255c67b73759501e
write-code-b <minjichoi.official@gmail.com>
2025-02-12 19:44:30 +0900

    chore: update Form Component

--- app/ui/Form.tsx
+++ app/ui/Form.tsx
@@ -1,29 +1,30 @@
 'use client';
 
+import { useState, useActionState } from 'react';
 import { createTodo } from '@/lib/actions';
 import Button from '@/ui/Button';
 import styles from '@/styles/Form.module.scss';
-import { useState, useActionState } from 'react';
 
 export default function Form() {
   const [formData, setFormData] = useState({ name: '' });
+  
   const [state, formAction] = useActionState(createTodo, formData);
 
   return (
     <form action={formAction} className={styles.checkListInputWrapper}>
-      <label htmlFor='checklist'></label>
+      <label htmlFor="checklist"></label>
       <div className={styles.checkListInputWrapper}>
         <div className={styles.checkListShadow}></div>
         <input
-          id='name'
-          name='name'
+          id="name"
+          name="name"
           className={styles.checkListInput}
-          type='text'
+          type="text"
           placeholder={'할 일을 입력해주세요'}
           required
         />
       </div>
-      <Button label='추가하기' state={'submit'} noData={false} />
+      <Button label="추가하기" state={'submit'} isFontWhite={false} />
     </form>
-  );
+  )
 }
