b2a6a8c80a739732e5a2f2aa249fad37086cd5fe
write-code-b <minjichoi.official@gmail.com>
2025-02-12 19:41:31 +0900

    chore: update Button Component

--- app/ui/Button.tsx
+++ app/ui/Button.tsx
@@ -2,26 +2,48 @@ import Image from 'next/image'
 import styles from '@/styles/Button.module.scss'
 
 interface ButtonProps {
-  noData?: boolean
+  isFontWhite?: boolean
   state?: string
   label?: string
+  backgroundColor?:string
   onClick?: () => void
 }
 
 export default function Button(props: ButtonProps) {
-  const { noData, state, label, onClick } = props
-  const fontColor = noData ? 'white' : 'black'
+  const { isFontWhite, state, label, backgroundColor, onClick } = props
+  const fontColor = isFontWhite ? 'white' : 'black'
 
   return (
     <div className={styles.buttonWrapper}>
       <div className={styles.buttonShadow}></div>
       <button
         className={styles.button}
-        style={{ color: `${fontColor}` }}
+        style={{ color: `${fontColor}`, backgroundColor: `${backgroundColor}` }}
         onClick={onClick}
       >
         {state === 'submit' && (
-          <Image src="/button/plus-dark-button.png" width={16} height={16} alt="" />
+          <Image
+            src="/button/plus-dark-button.png"
+            width={16}
+            height={16}
+            alt=""
+          />
+        )}
+        {state === 'edit' && (
+          <Image
+            src="/button/edit-dark-button.png"
+            width={16}
+            height={16}
+            alt=""
+          />
+        )}
+        {state === 'delete' && (
+          <Image
+            src="/button/delete-light-button.png"
+            width={16}
+            height={16}
+            alt=""
+          />
         )}
         <span>{label}</span>
       </button>
