9f52ce73ec041979cc9995cc1512cf56fcb230de
write-code-b <minjichoi.official@gmail.com>
2025-02-12 19:36:39 +0900

    feat: add fetchTodoById

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,18 +1,37 @@
 //Get All Todo data
 export async function fetchTodo() {
   try {
-    const url = `${process.env.NEXT_PUBLIC_API_KEY}/${process.env.NEXT_PUBLIC_TENANT_ID}/items`;
+    const url = `${process.env.NEXT_PUBLIC_API_KEY}/${process.env.NEXT_PUBLIC_TENANT_ID}/items`
 
     const checkList = await fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        return data;
-      });
+      .then((response) => response.json())
+      .then((data) => {
+        return data
+      })
 
-    return checkList;
+    return checkList
   } catch (error) {
-    console.error('[Fetch Todo]', error);
+    console.error('[Fetch Todo]', error)
 
-    throw new Error('Failed to fetch todo data.');
+    throw new Error('Failed to fetch todo data.')
+  }
+}
+
+//Get Todo data by Id
+export async function fetchTodoById(itemId: number) {
+  try {
+    const url = `${process.env.NEXT_PUBLIC_API_KEY}/${process.env.NEXT_PUBLIC_TENANT_ID}/items/${itemId}`
+
+    const checkList = await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        return data
+      })
+
+    return checkList
+  } catch (error) {
+    console.error('[Fetch Todo by Id]', error)
+
+    throw new Error('Failed to fetch todo by Id data.')
   }
 }
