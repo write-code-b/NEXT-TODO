270a0d4e0c9d35476a51fe58d5abd5bbdfc6ca7e
write-code-b <minjichoi.official@gmail.com>
2025-02-18 17:05:51 +0900

    chore: update button style

--- app/ui/Button.tsx
+++ app/ui/Button.tsx
@@ -5,19 +5,23 @@ interface ButtonProps {
   isFontWhite?: boolean
   state?: string
   label?: string
-  backgroundColor?:string
+  isCompleted?: boolean
+  backgroundColor?: string
   onClick?: () => void
 }
 
 export default function Button(props: ButtonProps) {
-  const { isFontWhite, state, label, backgroundColor, onClick } = props
+  const { isFontWhite, state, label, backgroundColor, onClick, isCompleted } =
+    props
   const fontColor = isFontWhite ? 'white' : 'black'
 
   return (
-    <div className={styles.buttonWrapper}>
+    <div
+      className={`${styles.buttonWrapper} ${state === 'submit' && styles.hideLabel}`}
+    >
       <div className={styles.buttonShadow}></div>
       <button
-        className={styles.button}
+        className={`${styles.button} ${isCompleted && styles.editable}`}
         style={{ color: `${fontColor}`, backgroundColor: `${backgroundColor}` }}
         onClick={onClick}
       >
